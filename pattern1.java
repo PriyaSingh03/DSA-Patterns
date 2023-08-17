2ef9b728e0ffc0d9bbe8e609a1d144f77559c0b0
PriyaSingh03 <singhpriyaangle@gmail.com>
2023-08-17 23:22:17 +0530

    pattern

--- pattern1.java
+++ pattern1.java
@@ -21,15 +21,16 @@ public class pattern1 {
         pattern15(5);
         pattern16(5);
         pattern17(5);
+        pattern18(5);
+        pattern19(5);
+        pattern20(5);
+        pattern21(5);
+        pattern22(5);
+        pattern23(5);
+        pattern24(5);
     }
 
-    /*
-    *****
-    *****
-    *****
-    *****
-    */
-
+    // Question 1
     static void pattern1(int n) {
         System.out.println("First Pattern: ");
         for (int i = 1; i <= n; i++) {
@@ -40,13 +41,7 @@ public class pattern1 {
         }
     }
 
-    /*
-     *
-     **
-     ***
-     ****
-     *****
-     */
+    // Question 2
     static void pattern2(int n) {
         System.out.println("Second Pattern: ");
         for (int i = 1; i <= n; i++) {
@@ -57,13 +52,7 @@ public class pattern1 {
         }
     }
 
-    /*
-    *****
-    ****
-    ***
-    **
-    *
-    */
+    // Question 3
     static void pattern3(int n) {
         System.out.println("Third Pattern: ");
         for (int i = 1; i <= n; i++) {
@@ -74,13 +63,7 @@ public class pattern1 {
         }
     }
 
-    /*
-     * 1
-     * 1 2
-     * 1 2 3
-     * 1 2 3 4
-     * 1 2 3 4 5
-     */
+    // Question 4
     static void pattern4(int n) {
         System.out.println("Fourth Pattern: ");
         for (int i = 1; i <= n; i++) {
@@ -91,17 +74,7 @@ public class pattern1 {
         }
     }
 
-    /*
-    *
-    **
-    ***
-    ****
-    *****
-    ****
-    ***
-    **
-    *
-    */
+    // Question 5
     static void pattern5(int n) {
         System.out.println("Fifth Pattern: ");
         // Upper half of the pattern
@@ -120,13 +93,7 @@ public class pattern1 {
         }
     }
 
-    /*
-         *
-        **
-       ***
-      ****
-     *****
-    */
+    // Question 6
     static void pattern6(int n) {
         System.out.println("Sixth pattern: ");
         for (int i = 1; i <= n; i++) {
@@ -140,13 +107,7 @@ public class pattern1 {
         }
     }
 
-    /*
-    *****
-     ****
-      ***
-       **
-        *
-    */
+    // Question 7
     static void pattern7(int n) {
         System.out.println("Seventh Pattern: ");
         for (int i = 1; i <= n; i++) {
@@ -160,13 +121,7 @@ public class pattern1 {
         }
     }
 
-    /*
-        *
-       ***
-      *****
-     *******
-    *********
-     */
+    // Question 8
     static void pattern8(int n) {
         System.out.println("Eighth pattern: ");
         for (int i = 1; i <= n; i++) {
@@ -180,13 +135,7 @@ public class pattern1 {
         }
     }
 
-    /*
-    *********
-     *******
-      *****
-       ***
-        *
-    */
+    // Question 9
     static void pattern9(int n) {
         System.out.println("Ninth pattern: ");
         for (int i = 1; i <= n; i++) {
@@ -200,12 +149,7 @@ public class pattern1 {
         }
     }
 
-    // *
-    // **
-    // ***
-    // ****
-    // *****
-
+    // Question 10
     static void pattern10(int n) {
         System.out.println("Tenth Pattern: ");
         for (int i = 1; i <= n; i++) {
@@ -219,13 +163,7 @@ public class pattern1 {
         }
     }
 
-    /*
-     * * * * *
-     * * * *
-     * * *
-     * *
-     *
-     */
+    // Question 11
     static void pattern11(int n) {
         System.out.println("Eleventh Pattern: ");
         for (int i = 1; i <= n; i++) {
@@ -239,18 +177,7 @@ public class pattern1 {
         }
     }
 
-    /*
-     * * * * *
-     * * * *
-     * * *
-     * *
-     *
-     *
-     * *
-     * * *
-     * * * *
-     * * * * *
-     */
+    // Question 12
     static void pattern12(int n) {
         System.out.println("12th Pattern: ");
         // Upper triangle
@@ -275,11 +202,7 @@ public class pattern1 {
         }
     }
 
-    // *
-    // * *
-    // * *
-    // * *
-    // *********
+    // Question 13
     static void pattern13(int n) {
         System.out.println("13th Pattern: ");
         for (int i = 0; i < n; i++) {
@@ -294,11 +217,7 @@ public class pattern1 {
         }
     }
 
-    // *********
-    // * *
-    // * *
-    // * *
-    // *
+    // Question 14
     static void pattern14(int n) {
         System.out.println("14th Pattern: ");
         for (int i = n; i >= 1; i--) {
@@ -316,15 +235,7 @@ public class pattern1 {
         }
     }
 
-    // *
-    // * *
-    // * *
-    // * *
-    // * *
-    // * *
-    // * *
-    // * *
-    // *
+    // Question 15
     static void pattern15(int n) {
         System.out.println("15th Pattern: ");
         for (int i = 1; i <= n; i++) {
@@ -356,11 +267,7 @@ public class pattern1 {
         }
     }
 
-    // 1
-    // 1 1
-    // 1 2 1
-    // 1 3 3 1
-    // 1 4 6 4 1
+    // Question 16
     static void pattern16(int n) {
         System.out.println("16th Pattern: ");
         for (int i = 0; i < n; i++) {
@@ -377,13 +284,7 @@ public class pattern1 {
         }
     }
 
-    // 1
-    // 212
-    // 32123
-    // 4321234
-    // 32123
-    // 212
-    // 1
+    // Question 17
     static void pattern17(int n) {
         System.out.println("17th Pattern: ");
         // Print upper half of the pyramid
@@ -420,15 +321,198 @@ public class pattern1 {
         }
     }
 
-    // **********
-    // **** ****
-    // *** ***
-    // ** **
-    // * *
-    // * *
-    // ** **
-    // *** ***
-    // **** ****
-    // **********
+    // Question 18
+    static void pattern18(int n) {
+        System.out.println("18th pattern: ");
+        // Upper half of the pattern
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j <= n; j++) {
 
+                System.out.print("*");
+            }
+            for (int k = 1; k <= 2 * i - 2; k++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j <= n; j++) {
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // Lower half of the pattern
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 2 * i - 2; j < 2 * n - 2; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    // Question 19
+    static void pattern19(int n) {
+        System.out.println("19th Pattern: ");
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int k = 2 * i - 2; k <= 2 * n - 2; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // lower half
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j <= n; j++) {
+
+                System.out.print("*");
+            }
+            for (int k = 1; k <= 2 * i - 2; k++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j <= n; j++) {
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    // Question 20
+    static void pattern20(int n) {
+        System.out.println("20th pattern: ");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - 1; j++) {
+                if (i == 1 || j == n - 1 || i == n || j == 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    // Question 21
+    static void pattern21(int n) {
+        System.out.println("21st pattern: ");
+        int num = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+
+    // Question 22
+    static void pattern22(int n) {
+        System.out.println("22nd pattern: ");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0)
+                    System.out.print("1");
+                else
+                    System.out.print("0");
+            }
+            System.out.println();
+        }
+    }
+
+    // Question 23
+    static void pattern23(int n) {
+        System.out.println("23rd Pattern: ");
+        // Upper half of the pattern
+        for (int i = 0; i < n; i++) {
+            // Print left asterisk
+            System.out.print("*");
+
+            // Print left spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print middle asterisk
+            System.out.print("*");
+
+            // Print middle spaces
+            for (int j = 0; j < 2 * (n - i - 1); j++) {
+                System.out.print(" ");
+            }
+
+            // Print right asterisk
+            System.out.print("*");
+
+            // Print right spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print right asterisk
+            System.out.print("*");
+
+            System.out.println();
+        }
+
+        // Lower half of the pattern
+        for (int i = n - 1; i >= 0; i--) {
+            // Print left asterisk
+            System.out.print("*");
+
+            // Print left spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print middle asterisk
+            System.out.print("*");
+
+            // Print middle spaces
+            for (int j = 0; j < 2 * (n - i - 1); j++) {
+                System.out.print(" ");
+            }
+
+            // Print right asterisk
+            System.out.print("*");
+
+            // Print right spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print right asterisk
+            System.out.print("*");
+
+            System.out.println();
+        }
+    }
+
+    // Question 24
+    static void pattern24(int n) {
+        System.out.println("24th Pattern: ");
+        for (int i = 1; i <= n; i++) {
+            /* Print trailing spaces */
+            for (int j = 1; j <= n - i; j++) {
+                System.out.println(" ");
+            }
+            /* Print stars and center spaces */
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n)
+                    System.out.print("*");
+                else
+                    System.out.println(" ");
+            }
+            System.out.println();
+        }
+    }
 }
